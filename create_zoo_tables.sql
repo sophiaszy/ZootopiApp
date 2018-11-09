c9b41e9de7c21a110f6f78ef26d322465bbd2b7d
Sophia Shen <sophiashen@Sophias-MacBook-Pro.local>
2018-11-09 00:29:57 -0800

    working create table statements

---
+++ create_zoo_tables.sql
@@ -0,0 +1,183 @@
+drop table performs;
+drop table caresfor;
+drop table trains;
+drop table trades;
+drop table eats;
+drop table show;
+drop table food;
+drop table keeper;
+drop table trainer;
+drop table employee;
+drop table employeecommunication;
+drop table animal;
+drop table animalfood;
+drop table habitat;
+drop table habitatbiome;
+drop table site;
+drop table zoo;
+
+
+create table zoo(
+    address varchar(40),
+    name char(20) not null,
+    phone int,
+    city char(20),
+    country char(20),
+    primary key (address)
+    );
+
+create table site(
+    site_id int,
+    location char(20),
+    used_for char(20),
+    zoo_address varchar(40),
+    primary key (site_id),
+    foreign key (zoo_address) references zoo ON DELETE CASCADE
+    );
+
+create table habitatbiome(
+    biome char(20),
+    temp int,
+    humidity int,
+    primary key (biome)
+    );
+
+create table habitat(
+    enclosure_id int,
+    biome char(20),
+    sq_ft int,
+    depth int,
+    site_id int not null,
+    primary key (enclosure_id),
+    foreign key (biome) references habitatbiome,
+    foreign key (site_id) references site ON DELETE CASCADE
+    );
+
+create table animalfood(
+    height int,
+    weight int,
+    species char(20),
+    sex char(20),
+    eat_freq_week int,
+    eat_amount int,
+    primary key (height, weight, species, sex)
+    );
+
+create table animal(
+    animal_id int,
+    name char(20),
+    age int,
+    sex char(20),
+    height int,
+    weight int,
+    species char(20),
+    enclosure_id int,
+    id_enclosure int,
+    primary key (animal_id),
+    foreign key (enclosure_id) references habitat ON DELETE SET NULL,
+    foreign key (height, weight, species, sex) references animalfood
+    );
+
+create table employeecommunication(
+    f_name char(20),
+    l_name char(20),
+    walkeetalkeeno int,
+    primary key (f_name, l_name)
+    );
+
+create table employee(
+    f_name char(20),
+    l_name char(20),
+    employee_id int,
+    pay int,
+    zoo_address varchar(40) not null,
+    primary key (employee_id),
+    foreign key (zoo_address) references zoo ON DELETE SET NULL,
+    foreign key (f_name, l_name) references employeecommunication
+    );
+
+create table keeper(
+    duty char(20),
+    employee_id int,
+    primary key (employee_id),
+    foreign key (employee_id) references employee ON DELETE CASCADE
+    );
+
+create table trainer(
+    speciality char(20),
+    employee_id int,
+    primary key (employee_id),
+    foreign key (employee_id) references employee ON DELETE CASCADE
+    );
+
+
+create table food(
+    food_id int,
+    stock_serving int,
+    date_purchased int,
+    date_expired int,
+    site_id int not null,
+    primary key (food_id),
+    foreign key (site_id) references site ON DELETE CASCADE
+    );
+
+
+create table show(
+    start_time int,
+    duration int,
+    name char(20),
+    type char(20),
+    site_id int not null,
+    primary key (start_time, name),
+    foreign key (site_id) references site ON DELETE CASCADE
+    );
+
+create table performs(
+	start_time int,
+    show_name char(20),
+	employee_id int,
+	animal_id int,
+	role char(50),
+	primary key (start_time, employee_id, animal_id),
+	foreign key (start_time, show_name) references show,
+	foreign key (employee_id) references employee,
+	foreign key (animal_id) references animal
+    );
+
+create table caresfor(
+	employee_id int,
+	animal_id int,
+	primary key (employee_id, animal_id),
+	foreign key (employee_id) references employee,
+	foreign key (animal_id) references animal
+    );
+
+create table trains(
+	employee_id int,
+	animal_id int,
+	skills char(50),
+	primary key (employee_id, animal_id),
+	foreign key (employee_id) references employee,
+	foreign key (animal_id) references animal
+    );
+
+create table trades(
+	zoo_from_address varchar(40),
+	zoo_to_address varchar(40),
+	animal_id int,
+	trade_date date,
+	primary key (zoo_from_address, zoo_to_address, animal_id),
+	foreign key (zoo_from_address) references zoo,
+	foreign key (zoo_to_address) references zoo,
+	foreign key (animal_id) references animal
+    );
+
+create table eats(
+	animal_id int,
+	food_id int,
+	primary key (animal_id, food_id),
+	foreign key (animal_id) references animal ON DELETE CASCADE,
+	foreign key (food_id) references food
+    );
+
+
