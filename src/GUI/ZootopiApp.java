4d84308c986c8034e1e30fb4a6288f93e1fb65f3
Brandon <btdjokic@gmail.com>
2018-11-10 20:51:28 -0800

    -added iml file to commit -initial code for app to start i.e. main fn

--- src/GUI/ZootopiApp.java
+++ src/GUI/ZootopiApp.java
@@ -1,14 +1,34 @@
 package GUI;
 
+
+import javax.swing.*;
+import java.awt.*;
+
 /*
 Main function file.  Launches the app
  */
 public class ZootopiApp {
 
+    public static JFrame                  window = new JFrame("ZootopiApp");
+    public static LoginPage                login = new LoginPage();
+    //public static ZooManager                user = null;
+    public static MainPage_Manager   main_window = null;
+
+
+    public ZootopiApp(){
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setPreferredSize(new Dimension(1500,1000));
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setVisible(true);
+        window.add(login);
+    }
+
     public static void main(String[] args ){
-        //initial commit
 
         //TODO: BRANDON -connect to database, and proceed to main page
+
+        ZootopiApp application = new ZootopiApp();
 
     }
 }
